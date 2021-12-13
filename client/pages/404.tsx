36ff5704aa4d9d90f83cdcd34c1820259ac08f3b
Jacob Sapoznikow <jacob1coder@gmail.com>
2021-12-13 21:49:27 +0000

    CI builds and more system setup

--- client/pages/404.tsx
+++ client/pages/404.tsx
@@ -1,26 +1,23 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import Image from 'next/image';
 import styles from '../styles/Home.module.css';
-import io from 'socket.io-client';
 import Link from 'next/link';
 
-const socket = io();
-
-const Home: NextPage = () => {
+const Error404: NextPage = () => {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Papyrus v6</title>
+        <title>Papyrus v6 | Error</title>
         <meta name="description" content="Papyrus v6: Instant messaging - done right." />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <Link href="/help">HELP</Link>
+        <p>404 | The page you requested could not be found.</p>
+        <Link href="/">Back to Home</Link>
       </main>
     </div>
   );
 }
 
-export default Home;
+export default Error404;
