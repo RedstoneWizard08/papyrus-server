5b6e7b25bffe76d4042d472c3eca0282400b5f31
Jacob Sapoznikow <jacob1coder@gmail.com>
2021-12-13 19:42:01 +0000

    ESLint on the backend

--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,7 @@
-const express = require("express");
-const http = require("http");
-const { Server } = require("socket.io");
-const proxy = require("express-http-proxy");
-const fs = require("fs");
-const path = require("path");
+import express from 'express';
+import http from 'http';
+import { Server } from 'socket.io';
+import proxy from 'express-http-proxy';
 
 const app = express();
 const port = process.env.PORT || 8900;
@@ -13,7 +11,14 @@ const frontend = proxy(`http://localhost:${frontendPort}`, {});
 const servlet = http.createServer(app);
 const io = new Server(servlet);
 
+io.on("connection", (socket) => {
+    console.log(`${socket.id} | Client connected!    | { id: ${socket.id}, ip: ${socket.handshake.address} }`);
+    socket.on("disconnect", () => {
+        console.log(`${socket.id} | Client disconnected! | { id: ${socket.id}, ip: ${socket.handshake.address} }`);
+    });
+});
 
+app.get("/*", frontend);
 
 servlet.listen(port, () => {
     console.log(`⚡️ [server] App listening on port ${port} (Frontend on port ${frontendPort}).`);
